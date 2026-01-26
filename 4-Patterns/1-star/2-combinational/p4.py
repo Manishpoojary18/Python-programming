dd17ebc611edc84c0316e92324e1208f74fef582
Manishpoojary18 <manishpoojary933@gmail.com>
2026-01-26 14:28:56 +0530

    add new programs

---
+++ 4-Patterns/1-star/2-combinational/p4.py
@@ -0,0 +1,12 @@
+n=int(input("Enter num:"))
+noc=1
+for i in range(1,(n*2)):
+    for k in range((n*2)-1,noc,-1):
+        print(" ",end="")
+    for j in range(1,(noc+1)):
+        print("*",end="")
+    print()
+    if i<n:
+        noc+=2
+    else:
+        noc-=2
---
+++ 4-Patterns/1-star/2-combinational/p6.py
@@ -0,0 +1,10 @@
+n=int(input("enter num:"))
+noc=n
+for i in range(1,(n*2)):
+    for j in range(1,noc+1):
+        print("*",end="")
+    print()
+    if i<n:
+        noc-=1
+    else:
+        noc+=1
