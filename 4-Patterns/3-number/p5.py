3922e71cba4e41cfbf504dc0aa23f2e6a07b5b2f
Manishpoojary18 <manishpoojary933@gmail.com>
2026-01-27 19:05:13 +0530

    add new programs

---
+++ 4-Patterns/3-number/p5.py
@@ -0,0 +1,11 @@
+n=int(input("Enter num:"))
+noc=n
+for i in range(1,(n*2)):
+    for j in range(1,noc+1):
+        print(j,end="")
+    print()
+    if i<n:
+        noc-=1
+    else:
+        noc+=1
+    
